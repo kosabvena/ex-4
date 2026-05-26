b93ad0720237b44360131cb968b58d4e90002222
kosabvena <kosabvena@gmail.com>
2026-05-26 15:48:28 +0300

    Add project files.

---
+++ ex 4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+string s = "біологія, алгебра, історія, географія, геометрія";
+
+string[] a = s.Split(' ');
+
+string temp = a[0];
+a[0] = a[1];
+a[1] = temp;
+
+string result = string.Join(" ", a);
+
+Console.WriteLine(result);
+Console.ReadKey();
